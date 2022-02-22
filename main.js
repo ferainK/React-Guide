ae191fa6cdc08bf722ac832f557f79433de3f17d
ferain <ferain@kakao.com>
2022-02-22 13:03:16 +0900

    2일차(회사)

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 //Class 사용해보기 (funciton은 생략)
 class ClassComponent extends React.Component {
-  render() {
-    return (<div>Hello</div>);
+  render(props) {
+    return <div>{this.props.message} Hello</div>
   }
 }
+
+ClassComponent.defaultProps ={
+  message: '값을 입력해주세요'
+}
+
 ReactDOM.render(<ClassComponent />, document.querySelector('#root1'))
+ReactDOM.render(<ClassComponent message='입력했네요.' />, document.querySelector('#root1-1'))
+
+
 
 // 태그 이름 문자열 type
 ReactDOM.render(
@@ -27,3 +35,31 @@ ReactDOM.render(
   React.createElement(React.Fragment, null, `type이 React.Fragment 입니다.`),
   document.querySelector('#root4')
 )
+
+//state
+class StateComponent extends React.Component{
+  state = {
+    count: 0,
+  }
+
+  render() {
+    return <div>{this.state.count} Hello</div>
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      //#1. 일반식
+      // this.setState({
+      //   count: this.state.count + 1,
+      // })
+
+      //#2. 함수식
+      this.setState((previousState) => {
+        const newState = { count: previousState.count + 1}
+        return newState
+      })
+    }, 1000)
+  }
+}
+
+ReactDOM.render(<StateComponent/>, document.querySelector('#root5'))
